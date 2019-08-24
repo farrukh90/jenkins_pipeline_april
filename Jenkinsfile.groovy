5daa8295a30ecd44eddc675dadb84fc9facc02b6
farrukh90 <farrukhsadykov@gmail.com>
2019-08-24 11:03:52 -0500

    removed extra line

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
     stage("Say Hello"){
         properties([pipelineTriggers([cron('* * * * * ')])])
-        sh "echo Hello ${NAME}",
-        sh "do something"
+        sh "echo Hello ${NAME}"
     }
 }
