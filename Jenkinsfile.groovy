4ee881c891d150f042441d0420cd99b327607103
farrukh90 <farrukhsadykov@gmail.com>
2019-08-24 11:28:28 -0500

    added apache install

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,9 @@
 node {
-    stage("Say Hello"){
+    stage("Pull Repo"){
         properties([parameters([string(defaultValue: '3.19.57.239', description: '''Dev: 3.19.57.239 QA: 3.15.185.26 Prod: 18.220.129.131''', name: 'Remote_instances', trim: true)])])
         git 'https://github.com/farrukh90/jenkins_april.git'
+    }
+    stage("Install Apache"){
+        sh "ssh   ec2-user@${Remote_instances}    sudo yum install httpd -y"
     }
 }
