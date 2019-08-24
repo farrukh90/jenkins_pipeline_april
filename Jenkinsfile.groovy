b9cccff6b026848d9fbd615876db2a9d658e059e
farrukh90 <farrukhsadykov@gmail.com>
2019-08-24 11:34:27 -0500

    added multiple steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,4 +6,13 @@ node {
     stage("Install Apache"){
         sh "ssh   ec2-user@${Remote_instances}    sudo yum install httpd -y"
     }
+    stage("Create Index.html"){
+        sh "scp  index.html  ec2-user@${Remote_instances}:/tmp"
+    }
+    stage("Move Files"){
+        sh "ssh   ec2-user@${Remote_instances}    sudo mv /tmp/index.html  /var/www/html/index.html"
+    }
+    stage("Restart httpd"){
+        sh "ssh   ec2-user@${Remote_instances} sudo systemctl restart  httpd"
+    }
 }
