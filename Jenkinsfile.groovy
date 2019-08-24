8e6746569a412bf3493eaf3c0dc5d7fdd8cd37f6
farrukh90 <farrukhsadykov@gmail.com>
2019-08-24 11:24:24 -0500

    added git and parameter

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 node {
     stage("Say Hello"){
+        properties([parameters([string(defaultValue: '3.19.57.239', description: '''Dev: 3.19.57.239 QA: 3.15.185.26 Prod: 18.220.129.131''', name: 'Remote_instances', trim: true)])])
         git 'https://github.com/farrukh90/jenkins_april.git'
     }
 }
